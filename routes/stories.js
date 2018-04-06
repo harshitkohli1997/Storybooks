fb52b8b86c80da92d2d6a83977f50c439e7e3b23
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-07 05:10:35 +0530

    listing comments section

--- routes/stories.js
+++ routes/stories.js
@@ -21,6 +21,7 @@ router.get('/show/:id', (req,res) => {
     _id: req.params.id
   })
   .populate('user')
+  .populate('comments.commentUser')
   .then(story =>{
     res.render('stories/show',{
       story:story
