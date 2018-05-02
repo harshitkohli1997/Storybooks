9614348b93d16330485026787d832dfe3b845386
harshit kohli <harshit.kohli1997@gmail.com>
2018-05-02 23:47:54 +0530

    resolving old err

--- routes/stories.js
+++ routes/stories.js
@@ -11,43 +11,45 @@ router.get('/', (req, res) => {
     .populate('user')
     .sort({date:'desc'})
     .then(stories => {
-        res.render('stories/index', {
+      res.render('stories/index', {
         stories: stories
       });
     });
 });
-// show single story
-router.get('/show/:id', (req,res) => {
+
+// Show Single Story
+router.get('/show/:id', (req, res) => {
   Story.findOne({
     _id: req.params.id
   })
   .populate('user')
   .populate('comments.commentUser')
-  .then(story =>{
-    res.render('stories/show',{
-      story:story
-    })
-  })
-})
+  .then(story => {
+    res.render('stories/show', {
+      story: story
+    });
+  });
+});
 
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add');
 });
-// edit story form
-router.get('/edit/:id',ensureAuthenticated, (req,res) =>{
-Story.findOne({_id:req.params.id
-})
-.then(story => {
-  if(story.user != req.user.id){
-   res.redirect('/stories');
-  }else
-  res.render('stories/edit',{
 
-    story:story 
-  });
-   
+// Edit Story Form
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
+  Story.findOne({
+    _id: req.params.id
   })
+  .then(story => {
+    if(story.user != req.user.id){
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', {
+        story: story
+      });
+    }
+  });
 });
 
 // Process Add Story
@@ -64,7 +66,7 @@ router.post('/', (req, res) => {
     title: req.body.title,
     body: req.body.body,
     status: req.body.status,
-    allowComments:req.body.allowComments,
+    allowComments:allowComments,
     user: req.user.id
   }
 
@@ -75,59 +77,61 @@ router.post('/', (req, res) => {
       res.redirect(`/stories/show/${story.id}`);
     });
 });
-//Edit process
-router.put('/:id',(req,res) => {
+
+// Edit Form Process
+router.put('/:id', (req, res) => {
   Story.findOne({
     _id: req.params.id
   })
-  .populate('user')
-  .then(story =>{
+  .then(story => {
     let allowComments;
+    
+    if(req.body.allowComments){
+      allowComments = true;
+    } else {
+      allowComments = false;
+    }
 
-  if(req.body.allowComments){
-    allowComments = true;
-  } else {
-    allowComments = false;
-  }
-  // new values
-  story.title = req.body.title;
-  story.body = req.body.body;
-  story.status = req.body.status;
-  story.allowComments = allowComments;
-story.save()
-.then(story => {
-  res.redirect('/dashboard');
-});
-});
-  });
-  //Delete story
-  router.delete('/:id', (req,res) => {
-       
-     Story.remove({_id:req.params.id})
-         .then(() => {
-           res.redirect('/dashboard');
-                    });
-  });
+    // New values
+    story.title = req.body.title;
+    story.body = req.body.body;
+    story.status = req.body.status;
+    story.allowComments = allowComments;
 
-  //add comment
-  router.post('/comment/:id',(req,res) => {
-    Story.findOne({
-      _id: req.params.id
-    })
-    .then( story => {
-      const newComment = {
-        commentBody: req.body.commentBody,
-        commentUser: req.user.id
-      }
-    story.comments.unshift(newComment);
     story.save()
-    .then(story => {
-      res.redirect(`/stories/show/${story.id}`);
-    })  
+      .then(story => {
+        res.redirect('/dashboard');
+      });
   });
+});
 
+// Delete Story
+router.delete('/:id', (req, res) => {
+  Story.remove({_id: req.params.id})
+    .then(() => {
+      res.redirect('/dashboard');
+    });
+});
 
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
+
+    // Add to comments array
+    story.comments.unshift(newComment);
+
+    story.save()
+      .then(story => {
+        res.redirect(`/stories/show/${story.id}`);
+      });
   });
-
+});
 
 module.exports = router;
